1c1e4c6273ce96acc5e5ebb7f8a08a822434079d
werleycordeiro <31543812+werleycordeiro@users.noreply.github.com>
2018-12-26 18:53:13 -0200

    Create Yiled Curve 3D.R

---
+++ Yiled Curve 3D.R
@@ -0,0 +1,21 @@
+data<-read.csv("https://www.dropbox.com/s/inpnlugzkddp42q/bonds.csv?dl=1",header = TRUE, sep = ";")
+library(xts)
+data <- read.csv("bonds.csv",header = TRUE, sep = ";")
+data <- as.matrix(data[,which(names(data)=="M3"):which(names(data)=="M120")])
+datas	<- seq(as.Date("1972/1/1"), by = "month", length.out = 348)
+juros	<- xts(data, order.by = datas)
+
+Maturidade<-t(t(juros))
+z<-Maturidade
+
+#-----------#
+# Imagem 3D #
+#-----------#
+col.pal<-colorRampPalette(c("midnightblue","blue","cyan3","cyan2","cyan1","cyan"))
+colors<-col.pal(100)
+# height of facets
+z.facet.center <- (z[-1, -1] + z[-1, -ncol(z)] + z[-nrow(z), -1] + z[-nrow(z), -ncol(z)])/4
+# Range of the facet center on a 100-scale (number of colors)
+z.facet.range<-cut(z.facet.center, 100)
+par(bg = "white")
+persp(z=z,x=seq(1970, 2000, length.out = nrow(z)),y=c(3,6,9,12,15,18,21,24,30,36,48,60,72,84,96,108,120),col=colors[z.facet.range],xlab ="Data", ylab ="Maturidade", zlab = "Taxa",theta=40,ticktype="detailed",phi=15,expand=0.4,nticks=5)
