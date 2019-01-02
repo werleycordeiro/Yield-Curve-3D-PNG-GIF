a979b583361726e203f05b963a720e242d1ff3e2
werleycordeiro <31543812+werleycordeiro@users.noreply.github.com>
2019-01-02 20:04:27 -0200

    Update Yiled Curve 3D.R

--- Yiled Curve 3D.R
+++ Yiled Curve 3D.R
@@ -18,3 +18,10 @@ z.facet.center <- (z[-1, -1] + z[-1, -ncol(z)] + z[-nrow(z), -1] + z[-nrow(z), -
 z.facet.range<-cut(z.facet.center, 100)
 par(bg = "white")
 persp(z=z,x=seq(1970, 2000, length.out = nrow(z)),y=c(3,6,9,12,15,18,21,24,30,36,48,60,72,84,96,108,120),col=colors[z.facet.range],xlab ="Data", ylab ="Maturidade", zlab = "Taxa",theta=40,ticktype="detailed",phi=15,expand=0.4,nticks=5)
+
+#-----#
+# GIF #
+#-----#
+
+library(animation)
+saveGIF(for(i in 1:90){ par(bg = "white"); persp(z=z,x=seq(1970, 2000, length.out = nrow(z)),y=c(3,6,9,12,15,18,21,24,30,36,48,60,72,84,96,108,120),col=colors[z.facet.range],xlab ="Data", ylab ="Maturidade", zlab = "Taxa",theta=i-40,ticktype="detailed",phi=15,expand=0.4,nticks=5) }, interval = 0.2)
