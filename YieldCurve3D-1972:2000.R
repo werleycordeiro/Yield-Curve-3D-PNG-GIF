50d5b5a3c9889f528a0e116cb0055aa4d42d673f
werleycordeiro <31543812+werleycordeiro@users.noreply.github.com>
2020-08-18 18:03:23 -0300

    Update YieldCurve3D-1972:2000.R

--- YieldCurve3D-1972:2000.R
+++ YieldCurve3D-1972:2000.R
@@ -4,8 +4,9 @@ data <- as.matrix(data[,which(names(data)=="M3"):which(names(data)=="M120")])
 datas	<- seq(as.Date("1972/1/1"), by = "month", length.out = 348)
 juros	<- xts(data, order.by = datas)
 
-Maturidade<-t(t(juros))
-z<-Maturidade
+Maturidade<-c(3,6,9,12,15,18,21,24,30,36,48,60,72,84,96,108,120)
+z<-t(t(juros))
+
 
 #-----------#
 # Imagem 3D #
@@ -17,7 +18,7 @@ z.facet.center <- (z[-1, -1] + z[-1, -ncol(z)] + z[-nrow(z), -1] + z[-nrow(z), -
 # Range of the facet center on a 100-scale (number of colors)
 z.facet.range<-cut(z.facet.center, 100)
 par(bg = "white")
-persp(z=z,x=seq(1970, 2000, length.out = nrow(z)),y=c(3,6,9,12,15,18,21,24,30,36,48,60,72,84,96,108,120),col=colors[z.facet.range],xlab ="Data", ylab ="Maturidade", zlab = "Taxa",theta=40,ticktype="detailed",phi=15,expand=0.4,nticks=5)
+persp(z=z,x=seq(1970, 2000, length.out = nrow(z)),y=Maturidade,col=colors[z.facet.range],xlab ="Data", ylab ="Maturidade", zlab = "Taxa",theta=40,ticktype="detailed",phi=15,expand=0.4,nticks=5)
 
 #-----#
 # GIF #
